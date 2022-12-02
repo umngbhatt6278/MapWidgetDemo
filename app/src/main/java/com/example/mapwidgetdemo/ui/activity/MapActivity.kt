9cf5e1c0fbcf8e536e7404e9dd9065fab1d5ab53
Sajid <sajid.suthar@goniyo.com>
2022-12-02 18:35:33 +0530

    Done: Widget added on home screen On click on widget open camera for video record Save the video with location in room db One check added in map activity that is user come from widget or not

--- app/src/main/java/com/example/mapwidgetdemo/ui/activity/MapActivity.kt
+++ app/src/main/java/com/example/mapwidgetdemo/ui/activity/MapActivity.kt
@@ -3,24 +3,34 @@ package com.example.mapwidgetdemo.ui.activity
 import android.Manifest
 import android.Manifest.permission.*
 import android.app.AlertDialog
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.location.LocationRequest
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.provider.Settings
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.example.mapwidgetdemo.R
 import com.example.mapwidgetdemo.ui.activity.database.MarkerViewModel
 import com.example.mapwidgetdemo.ui.activity.database.WordViewModelFactory
 import com.example.mapwidgetdemo.ui.activity.database.model.MarkerModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -29,9 +39,18 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 open class MapActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+    lateinit var videoUri: Uri
+    private lateinit var locationRequest: LocationRequest
 
     private lateinit var map: GoogleMap
     private val TAG = "mytag"
@@ -56,39 +75,69 @@ open class MapActivity : AppCompatActivity(), OnMapReadyCallback, LocationListen
         WordViewModelFactory((application as MainApplication).repository)
     }
 
+    private val contract = registerForActivityResult(ActivityResultContracts.CaptureVideo()) {
+        if (it) {
+            fetchCurrentLocation()
+        }
+    }
+
+    private fun fetchCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            val manager: LocationManager =
+                getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+            } else {
+                fusedLocationClient.lastLocation
+                    .addOnSuccessListener { location ->
+                        saveVideoWithLocation(location.latitude, location.longitude)
+                    }
+                    .addOnFailureListener {
+                        Log.e("TAG", "fetchLocation() --> Fail")
+                    }
+            }
+        }
+    }
+
+    private fun saveVideoWithLocation(latitude: Double, longitude: Double) {
+        wordViewModel.insert(MarkerModel(latitude = latitude,
+            longitude = longitude,
+            videopath = videoUri.toString()))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        Log.e(TAG,"onCreate() --> ${intent.getBooleanExtra("IS_FROM_WIDGET", false)}")
+
+        if (intent.getBooleanExtra("IS_FROM_WIDGET", false)){
+            videoUri = createFileUri()!!
+            if (checkCameraHardware(this)) {
+                contract.launch(videoUri)
+            }
+        }
+
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult ?: return
+                if (locationResult.locations.isNotEmpty()){
+                    for (location in locationResult.locations) {
+                        saveVideoWithLocation(location.latitude, location.longitude)
+                    }
+                }
+            }
+        }
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
-
-
-        var word = MarkerModel(
-            latitude = 23.012043440161523, longitude = 72.50499295485864,
-            videopath = ""
-        )
-        wordViewModel.insert(word)
-
-        word = MarkerModel(
-            latitude = 23.028979364949823, longitude = 72.49916689205622,
-            videopath = ""
-        )
-        wordViewModel.insert(word)
-
-        word = MarkerModel(
-            latitude = 23.034943022650506, longitude = 72.49775068574698,
-            videopath = ""
-        )
-        wordViewModel.insert(word)
-
-        word = MarkerModel(
-            latitude = 23.02854491599756, longitude = 72.51247064829457,
-            videopath = ""
-        )
-        wordViewModel.insert(word)
-
-
 
         if (!checkPermission()) {
             requestPermission()
@@ -127,6 +176,22 @@ open class MapActivity : AppCompatActivity(), OnMapReadyCallback, LocationListen
 
     }
 
+    private fun checkCameraHardware(context: Context): Boolean {
+        return context.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)
+    }
+
+    private fun createFileUri(): Uri? {
+        val sdf = SimpleDateFormat("dd_M_yyyy_hh_mm_ss", Locale.ENGLISH)
+        val currentDate = sdf.format(Date())
+        val video = File(applicationContext.filesDir, "$currentDate.mp4")
+        return FileProvider.getUriForFile(
+            applicationContext,
+            "com.example.mapwidgetdemo.fileProvider",
+            video
+        )
+    }
+
+
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
@@ -150,33 +215,35 @@ open class MapActivity : AppCompatActivity(), OnMapReadyCallback, LocationListen
             words.let {
                 val data = it
 
-                for (i in data?.indices!!) {
-                    builder.include(LatLng(data?.get(i)?.latitude!!, data?.get(i)?.longitude!!))
-                    createMarker(
-                        LatLng(data[i].latitude, data[i].longitude),
-                        "Title One",
-                        "Snippet1$i"
-                    )
-                }
+                if (!data.isNullOrEmpty()){
+                    for (i in data?.indices!!) {
+                        builder.include(LatLng(data?.get(i)?.latitude!!, data?.get(i)?.longitude!!))
+                        createMarker(
+                            LatLng(data[i].latitude, data[i].longitude),
+                            "Title One",
+                            "Snippet1$i"
+                        )
+                    }
 
-                val bounds = builder.build();
-                val handler = Handler()
-                handler.postDelayed(Runnable {
-                    map.animateCamera(
-                        zoomWidth?.let { width: Int ->
-                            zoomHeight?.let { height ->
-                                zoomPadding?.let { padding ->
-                                    CameraUpdateFactory.newLatLngBounds(
-                                        bounds,
-                                        width,
-                                        height,
-                                        padding.toInt()
-                                    )
+                    val bounds = builder.build();
+                    val handler = Handler()
+                    handler.postDelayed(Runnable {
+                        map.animateCamera(
+                            zoomWidth?.let { width: Int ->
+                                zoomHeight?.let { height ->
+                                    zoomPadding?.let { padding ->
+                                        CameraUpdateFactory.newLatLngBounds(
+                                            bounds,
+                                            width,
+                                            height,
+                                            padding.toInt()
+                                        )
+                                    }
                                 }
                             }
-                        }
-                    )
-                }, 500)
+                        )
+                    }, 500)
+                }
             }
         }
 
@@ -201,13 +268,16 @@ open class MapActivity : AppCompatActivity(), OnMapReadyCallback, LocationListen
         val location = ContextCompat.checkSelfPermission(applicationContext, ACCESS_FINE_LOCATION)
         val write = ContextCompat.checkSelfPermission(applicationContext, WRITE_EXTERNAL_STORAGE)
         val read = ContextCompat.checkSelfPermission(applicationContext, READ_EXTERNAL_STORAGE)
+        val camera = ContextCompat.checkSelfPermission(applicationContext, CAMERA)
+        val audio = ContextCompat.checkSelfPermission(applicationContext, RECORD_AUDIO)
         return location == PackageManager.PERMISSION_GRANTED && write == PackageManager.PERMISSION_GRANTED && read == PackageManager.PERMISSION_GRANTED
+                && camera == PackageManager.PERMISSION_GRANTED && audio == PackageManager.PERMISSION_GRANTED
     }
 
     open fun requestPermission() {
         ActivityCompat.requestPermissions(
             this,
-            arrayOf(ACCESS_FINE_LOCATION, WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE),
+            arrayOf(ACCESS_FINE_LOCATION, WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE, CAMERA, RECORD_AUDIO),
             REQUEST_LOCATION_PERMISSION
         )
     }
@@ -234,7 +304,7 @@ open class MapActivity : AppCompatActivity(), OnMapReadyCallback, LocationListen
                                         requestPermissions(
                                             arrayOf(
                                                 ACCESS_FINE_LOCATION, WRITE_EXTERNAL_STORAGE,
-                                                READ_EXTERNAL_STORAGE
+                                                READ_EXTERNAL_STORAGE, CAMERA, RECORD_AUDIO
                                             ),
                                             REQUEST_LOCATION_PERMISSION
                                         )
@@ -265,5 +335,14 @@ open class MapActivity : AppCompatActivity(), OnMapReadyCallback, LocationListen
         val cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10f)
 //        map.animateCamera(cameraUpdate)
         locationManager?.removeUpdates(this);
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
     }
 }
---
+++ app/src/main/java/com/example/mapwidgetdemo/widgetprovider/RecordVideoWidget.kt
@@ -0,0 +1,49 @@
+package com.example.mapwidgetdemo.widgetprovider
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+import com.example.mapwidgetdemo.R
+import com.example.mapwidgetdemo.ui.activity.MapActivity
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class RecordVideoWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    val intent = Intent(context, MapActivity::class.java)
+    intent.putExtra("IS_FROM_WIDGET", true)
+    val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+    // Construct the RemoteViews object
+    val views = RemoteViews(context.packageName, R.layout.record_video_widget)
+    views.setOnClickPendingIntent(R.id.appwidget_video, pendingIntent)
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
